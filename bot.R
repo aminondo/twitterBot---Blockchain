ba919ef075d54121d5212966967cf783273f9f04
= <=>
2017-06-12 18:51:25 +0000

    removed log from bot.R

--- bot.R
+++ bot.R
@@ -1,7 +1,7 @@
 library(twitteR)
 library(dplyr)
 library(rvest)
-library(mgcv)
+#library(mgcv)
 
 consumer_key = 'UiaBOjq2a6bgQrd3AwtvKJZWD'
 consumer_secret = 'qiDxxpyLz6PieqNw8YJsfvanBMfCZt5uMx2wK9pfTlzIMl1EbU'
@@ -14,7 +14,7 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 #------------------------------------------------------------------------
 news <- read_html("http://www.the-blockchain.com/docs/index.php") %>% html_nodes(".thumbtext") 
 #log = list()
-log = readRDS("log.rds")
+#log = readRDS("log.rds")
 
 hrefs = html_children(news) %>% html_children() %>% html_attr("href")
 hrefs = hrefs[!is.na(hrefs)]
@@ -27,14 +27,14 @@ urls = lapply(1:length(hrefs), function(x) {
 #choose a random article
 num = sample(1:length(urls),1)
 twt = sprintf("Check this article out: %s\n\n#blockchain #twitterBots",urls[[num]])
-while (twt %in% log) {
-  print("running")
-  num = sample(1:length(urls),1)
-  twt = sprintf("Check this article out\n: %s\n\n#blockchain #twitterBots",urls[[num]])
-}
+#while (twt %in% log) {
+#  print("running")
+#  num = sample(1:length(urls),1)
+#  twt = sprintf("Check this article out\n: %s\n\n#blockchain #twitterBots",urls[[num]])
+#}
 
 status = tweet(twt,bypassCharLimit=T)
 #deleteStatus(status)
-log = c(log,twt)
-saveRDS(log,"log.rds")
+#log = c(log,twt)
+#saveRDS(log,"log.rds")
 
